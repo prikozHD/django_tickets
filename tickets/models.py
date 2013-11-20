8f2609f694b18852f654b169b786bbd15928b147
prikozhd <prikozhd@gmail.com>
2013-11-21 01:32:53 +0200

    commit

--- tickets/models.py
+++ tickets/models.py
@@ -14,10 +14,10 @@ class TimeModel(models.Model):
 
 
 class Departament(TimeModel):
-    name = models.CharField(max_length=200, verbose_name=_("Departament"), db_index=True, unique=True)
-    email = models.EmailField(max_length=200, verbose_name=_("E-mail"), unique=True)
+    name = models.CharField(max_length=200, verbose_name=_("Departament name"), db_index=True, unique=True)
+    email = models.EmailField(max_length=200, verbose_name=_("Departament E-mail"), unique=True)
     description = models.TextField(verbose_name=_("Departament description"))
-    locale = models.CharField(max_length=3, choices=DJANGO_LANGUAGES, blank=True)
+    locale = models.CharField(max_length=3, choices=DJANGO_LANGUAGES, verbose_name=_("Departament Locale"),  blank=True)
 
     def __unicode__(self):
         return u"{0} {1} {2}".format(self.name, self.created, self.modified)
