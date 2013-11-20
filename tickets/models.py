33049940af945e996afe2d6be5d1986058aaa9a9
prikozhd <prikozhd@gmail.com>
2013-11-21 01:26:40 +0200

    commit

--- tickets/models.py
+++ tickets/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
-from django.conf.global_settings import LANGUAGES
+from django.conf.global_settings import LANGUAGES as DJANGO_LANGUAGES
 
 
 class TimeModel(models.Model):
@@ -14,11 +14,10 @@ class TimeModel(models.Model):
 
 
 class Departament(TimeModel):
-    DEPARTAMENT_LOCALE_CHOICE = LANGUAGES
     name = models.CharField(max_length=200, verbose_name=_("Departament"), db_index=True, unique=True)
     email = models.EmailField(max_length=200, verbose_name=_("E-mail"), unique=True)
     description = models.TextField(verbose_name=_("Departament description"))
-    locale = models.CharField(max_length=3, choices=DEPARTAMENT_LOCALE_CHOICE, blank=True)
+    locale = models.CharField(max_length=3, choices=DJANGO_LANGUAGES, blank=True)
 
     def __unicode__(self):
         return u"{0} {1} {2}".format(self.name, self.created, self.modified)
